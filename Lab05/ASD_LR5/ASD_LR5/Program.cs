032234f2ee6b8e8a0c07472e1b6e1fe2ed4664b7
NikishaAO <92879031+NikishaAO@users.noreply.github.com>
2022-05-19 20:18:51 +0300

    Lab finished

---
+++ Lab05/ASD_LR5/ASD_LR5/Program.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace ASD_LR5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choise, m = 4, n = 4, k = 2 ;
+            int[,] Matrix = new int[4, 4] { { 1, 12, 15,11 }, { 4, 2, 9, 16}, { 10, 3, 8,6 },{ 5, 7, 14,13} };
+
+
+            WriteLine("1 - згенерувати масив, iнше - контрольний приклад");
+            choise = Convert.ToInt32(ReadLine());
+            if(choise == 1)
+            {
+                Write("m = ");
+                 m = Convert.ToInt32(ReadLine());
+                Write("n = ");
+                 n = Convert.ToInt32(ReadLine());
+
+                Matrix = GenArr(m, n);
+                Write("k = ");
+                 k = Convert.ToInt32(ReadLine());
+            }
+         
+
+
+
+            WriteArr(Matrix,k);
+
+            int[] List = To1D(Matrix);            
+
+         
+
+            List<int> Al = new List<int>();
+            List<int> Else = new List<int>();
+            List<int> Index = new List<int>();
+
+            for(int i = 0; i < List.Length; i++)
+            {
+                if (List[i] % k == 0)
+                {
+                    Al.Add(List[i]);
+                    Index.Add(i);
+                }
+                else
+                    Else.Add(List[i]);                  
+            }
+
+            if (Al.Count < m)
+            {
+                Al = InsertionDec(Al);
+                Else = SortInc(Else, 0, Else.Count - 1);
+
+            }
+            else if (Else.Count < m)
+            {
+                Al = SortDec(Al, 0, Al.Count - 1);
+                Else = InsertionInc(Else);
+            }
+            else
+            {
+                Al = SortDec(Al, 0, Al.Count - 1);
+                Else = SortInc(Else, 0, Else.Count - 1);
+            }
+
+            int a = 0;
+            int b = 0;
+
+
+            for (int x = 0; x < List.Length; x++)
+            {
+                if (Index.Contains(x))
+                {
+                    List[x] = Al[a];
+                    a++;
+                }
+                else
+                {
+                    List[x] = Else[b];
+                    b++;
+                }
+            }
+
+            a = 0;
+
+            for (int j = 0; j < n; j++)
+            for(int i = 0; i < m; i++)
+            {
+                    Matrix[i,j]= List[a];
+                    a++;
+            }
+
+            WriteLine();
+            WriteArr(Matrix, k);
+        }
+
+        static int[,] GenArr(int m, int n)
+        {
+            int[,] Arr = new int[m, n];
+            List<int> Con = new List<int>();
+            int num;
+            Random rnd = new Random();
+
+            for (int j = 0; j < n; j++)
+                for (int i = 0; i < m; i++)
+                {
+                    do
+                    {
+                        num = rnd.Next(100);
+                    } while (Con.Contains(num));
+                    Arr[i, j] = num;
+                    Con.Add(num);      
+                }
+
+            return Arr;
+        }
+
+        static void WriteArr(int[,] Arr, int k)
+        {
+            for (int j = 0; j < Arr.GetLength(1); j++)
+            {
+                WriteLine();
+                for (int i = 0; i < Arr.GetLength(0); i++)
+                {
+                    if (Arr[i, j] % k == 0)
+                    {
+                        ForegroundColor = ConsoleColor.Yellow;
+                        Write(Arr[i, j] + " ");
+                        ResetColor();
+                    }
+                    else
+                    {
+                        ForegroundColor = ConsoleColor.Blue;
+                        Write(Arr[i, j] + " ");
+                        ResetColor();
+                    }
+                }
+            }
+        }
+
+
+        static int[] To1D(int[,] Arr)
+        {
+            int[] NewArr = new int[Arr.GetLength(0) * Arr.GetLength(1)];
+            int p = 0;
+
+            for (int j = 0; j < Arr.GetLength(1); j++)
+                for (int i = 0; i < Arr.GetLength(0); i++)
+                {
+                    NewArr[p] = Arr[i, j];
+                    p++;
+                }
+
+            return NewArr;
+        }
+
+        static List<int> SortInc(List<int> Arr, int low, int high)
+        {
+
+            int p;
+            if (low < high)
+            {
+                p = LomutoPartInc(Arr, low, high);
+                Arr = SortInc(Arr, low, p-1);
+                Arr = SortInc(Arr, p + 1, high);
+            }
+
+
+            return Arr;
+            
+        }
+
+        static List<int> SortDec(List<int> Arr, int low, int high)
+        {
+
+            int p;
+            if (low < high)
+            {
+                p = LomutoPartDec(Arr, low, high);
+                Arr = SortDec(Arr, low, p - 1);
+                Arr = SortDec(Arr, p + 1, high);
+            }
+
+
+            return Arr;
+
+        }
+        static int LomutoPartInc(List<int> Arr, int low, int high)
+        {
+           int  pivot = Arr[high];
+            int i = low-1;
+            int buf;
+            for( int j = low; j < high; j++)
+            {
+                if(Arr[j] < pivot)
+                {
+                    i++;
+
+                    buf = Arr[i];
+                    Arr[i] = Arr[j];
+                    Arr[j] = buf;
+
+                }
+            }
+            buf = Arr[high];
+            Arr[high] = Arr[i+1];
+            Arr[i+1] = buf;
+
+            return i+1;
+        }
+
+        static int LomutoPartDec(List<int> Arr, int low, int high)
+        {
+            int pivot = Arr[high];
+            int i = low - 1;
+            int buf;
+            for (int j = low; j < high; j++)
+            {
+                if (Arr[j] > pivot)
+                {
+                    i++;
+
+                    buf = Arr[i];
+                    Arr[i] = Arr[j];
+                    Arr[j] = buf;
+
+                }
+            }
+            buf = Arr[high];
+            Arr[high] = Arr[i + 1];
+            Arr[i + 1] = buf;
+
+            return i + 1;
+        }
+
+        static List<int> InsertionInc(List<int> List)
+        {
+            for(int i = 0; i < List.Count; i++)
+            {
+                int j = i - 1;
+                int buf = List[i];
+                while(j >= 0 && List[j] > buf )
+                {
+                    List[j + 1] = List[j];
+                    j--;
+                }
+                List[j + 1] = buf;
+            }
+
+            return List;
+        }
+
+        static List<int> InsertionDec(List<int> List)
+        {
+            for (int i = 0; i < List.Count; i++)
+            {
+                int j = i - 1;
+                int buf = List[i];
+                while (j >= 0 && List[j] < buf )
+                {
+                    List[j + 1] = List[j];
+                    j--;
+                }
+                List[j + 1] = buf;
+            }
+
+            return List;
+        }
+    }
+
+
+
+
+}
---
+++ Lab06/ASD_LR6/ASD_LR6/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using static System.Console;
+
+namespace ASD_LR6
+{
+    class Program
+    {
+        struct Queue
+        {
+            public int head;
+            public int tail;
+            public int size;
+            public int[] Arr;
+
+            public Queue(int head, int tail, int size,int[] Arr)
+            {
+                this.head = head;
+                this.tail = tail;
+                this.size = size;
+                this.Arr = Arr;
+            }
+
+            public Queue InitQueue(int El, int S)
+            {
+                this.Arr = new int[S];
+                this.Arr[0] = El;
+                this.tail = 0;
+                this.head = 0;
+                this.size = 1;
+
+                return this;
+
+            }
+            public void WT() => WriteLine(this.tail);
+            
+            public Queue enqueue(int El)
+            {
+                if (this.Arr[(this.tail + 1) % this.Arr.Length] == 0)
+                {
+                    this.tail = (this.tail + 1) % this.Arr.Length;
+                    this.Arr[tail] = El;
+                    this.size++;
+                }
+                else
+                {
+                    WriteLine("Черга переповнена\nЧерга збiльшена, продовжiть введення даних ");
+                    IncreaseLength();
+                    
+                }                              
+                return this;
+
+            }
+            public Queue dequeue()
+            {
+                this.Arr[head] = 0;
+                this.head = (this.head + 1) % this.Arr.Length;
+
+                this.size--;
+                return this;
+            }
+
+            public bool isQueueEmpty()
+            {
+                if (this.size > 0)
+                    return false;
+                else
+                    return true;                      
+            }
+
+            public void GetSize() => WriteLine(this.size);
+
+            public void WriteQueue()
+            {
+                WriteLine("Поточна черга: ");
+                foreach (int El in this.Arr)
+                {
+                    Write(El + " ");
+                }
+                WriteLine();
+            }
+
+            public void Head() => WriteLine(this.head);
+
+            public Queue IncreaseLength()
+            {
+                int[] NewArr = new int[this.Arr.Length + 5];
+                if (this.tail > this.head)
+                {
+                    for(int i = 0; i < this.Arr.Length; i++)
+                    {
+                        NewArr[i] = this.Arr[i];
+                    }                    
+                }
+                else
+                {
+                    for(int i = 0; i <= tail; i++)
+                    {
+                        NewArr[i] = this.Arr[i];
+                    }
+                    for(int j = NewArr.Length -1; j-5 >= head; j--)
+                    {
+                        NewArr[j] = this.Arr[j - 5];
+                    }
+                    this.head = this.head + 5;
+                }
+
+                this.Arr = NewArr;
+                return this;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Queue Q = new Queue();
+            WriteLine(" 1- контрольний приклад, інше - ручний режим");
+            int choise = Convert.ToInt32(ReadLine());
+            if (choise == 1)
+            {
+                Q = Q.InitQueue(1, 5);
+                Q.WriteQueue();
+                WriteLine("4");
+                Q.enqueue(4);
+                Q.WriteQueue();
+                WriteLine("6");
+                Q.enqueue(6);
+                Q.WriteQueue();
+                WriteLine("2");
+                Q.enqueue(2);
+                Q.WriteQueue();
+                WriteLine("0");
+                for (int i = 0; i < 3; i++)
+                {
+                    Q.dequeue();
+                    Q.WriteQueue();
+                }
+                WriteLine("3");
+                Q.enqueue(3);
+                Q.WriteQueue();
+                WriteLine("7");
+                Q.enqueue(7);
+                Q.WriteQueue();
+                WriteLine("5");
+                Q.enqueue(5);
+                Q.WriteQueue();
+                WriteLine("3");
+                Q.enqueue(3);
+                Q.WriteQueue();
+                WriteLine("1");
+                Q.enqueue(1);
+                Q.WriteQueue();
+                WriteLine("4");
+                Q.enqueue(4);
+                Q.WriteQueue();
+                WriteLine("2");
+                Q.enqueue(2);
+                Q.WriteQueue();
+                WriteLine("0");
+                for (int i = 0; i < 3; i++)
+                {
+                    Q.dequeue();
+                    Q.WriteQueue();
+                }
+                WriteLine("0");
+                for (int i = 0; i < 3; i++)
+                {
+                    Q.dequeue();
+                    Q.WriteQueue();
+                }
+                WriteLine("0");
+                for (int i = 0; i < 3; i++)
+                {
+                    Q.dequeue();
+                    Q.WriteQueue();
+                }
+
+
+
+            }
+            else
+            {
+                WriteLine("Введiть перший елемент");
+                char inputC = Convert.ToChar(ReadLine());
+                if (Char.IsDigit(inputC))
+                {
+                    Q = Q.InitQueue(inputC  - 48, 5);
+                    Q.WriteQueue();
+                    while (Q.size > 0)
+                    {
+                        inputC = Convert.ToChar(ReadLine());
+                        if (Char.IsDigit(inputC))
+                        {
+                            int input = Convert.ToInt32(inputC - 48);
+
+                            if (input != 0)
+                            {
+                                Q.enqueue(input);
+                                Q.WriteQueue();
+                            }
+                            else
+                            {
+                                for (int i = 0; i < 3; i++)
+                                {
+                                    Q.dequeue();
+                                    Q.WriteQueue();
+                                }
+                            }
+                        }
+                        else
+                            WriteLine("Некоректне значення");
+                    }
+                }
+                else
+                    WriteLine("Некоректне значення");
+            }
+
+        }
+    }
+}
